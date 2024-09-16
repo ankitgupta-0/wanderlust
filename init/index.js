a60235aa47c99332e186d4c60522619b27ba1e3c
Ankit Gupta <guptaankitiil5@gmail.com>
2024-09-16 21:28:58 +0530

    Initial commit

--- init/index.js
+++ init/index.js
@@ -4,12 +4,12 @@ const Listing = require("../models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then(() => {
-    console.log("connected to DB");
-})
-.catch((err) => {
-    console.log(err);
-});
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
     await mongoose.connect(MONGO_URL);
